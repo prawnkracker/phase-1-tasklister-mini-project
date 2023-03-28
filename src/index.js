e5041c75e168a90e9645856dacfb683e72c6820e
Dennis Demir <dennisjdemir@gmail.com>
2023-03-28 14:22:40 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.querySelector('form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    buildToDo(e.target.new_description.value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDeleteButton)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDeleteButton(e){
+  e.target.parentNode.remove()
+}
